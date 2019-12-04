567acc27ff5191aeacc25f45fb9b01873698d123
Nguyen Hoang <hoangtrankhoinguyen@gmail.com>
2019-12-05 01:49:09 +0800

    Question 4
    Change permission of webroot forlder to make sure  it work on Linux

--- app/Model/FileUpload.php
+++ app/Model/FileUpload.php
@@ -1,5 +1,17 @@
 <?php
 
-class FileUpload extends AppModel {
-
+class FileUpload extends AppModel
+{
+  public $validate = [
+    'file' => [
+      'upload-file' => [
+        'rule' => ['uploadError'],
+        'message' => 'Error uploading file'
+      ],
+      'mimetype' => [
+        'rule' => ['mimeType', ['text/csv', 'text/plain']],
+        'message' => 'MIME type error'
+      ]
+    ]
+  ];
 }
